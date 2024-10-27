47ad65b6a7dc13b7c68f17bc1377d9a523879396
5ohue <86558263+5ohue@users.noreply.github.com>
2024-10-27 12:57:30 +0300

    Add `soh_ui` crate (empty)

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@ pub use soh_log as log;
 #[cfg(feature = "vk")]
 pub use soh_vk as vk;
 
+#[cfg(feature = "ui")]
+pub use soh_ui as ui;
+
 #[cfg(feature = "thread")]
 pub use soh_thread as thread;
 
